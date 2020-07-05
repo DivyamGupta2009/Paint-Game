76d12d5824e5453d50b2a0d0040cd1c8acd031dd
DivyamGupta2009 <62936715+DivyamGupta2009@users.noreply.github.com>
2020-07-05 13:09:02 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,38 +1,44 @@
-var pointer, database, pointerPositionRef;
+var  database;
+var drawing = [];
+var currentPath = [];
 
 function setup(){
     database = firebase.database();
     createCanvas(500,500);
-    pointer = createSprite(mouseX, mouseY, 10, 10);
-    pointer.shapeColor = "black";
-    pointerPositionRef = database.ref("pointer/Position");
-    pointerPositionRef.on("value", readPosition);
+    canvas.mousePressed(startPath);
+    canvas.parent('canvascontainer');
+    //canvas.mouseReleased(endPath);
 }
+
+function startPath(){
+    currentPath =  [];
+    drawing.push(currentPath);
+}
+
+//function endPath(){
+//}
 
 function draw(){
-    background("white");
-    writePosition();
-    readPosition();
-    
-    if(mousePressed()){
-    mouseDragged();
-    }
-   
-    drawSprites();
-}
+    background(0);
+ if(mouseIsPressed){
+     var point = {
+         x: mouseX,
+         y:mouseY
+     }
+   currentPath.push(point);
+ }
 
-function writePosition(x,y){
-    database.ref("pointer/Position").set({
-        'x':Position.x+x,
-        'y':Position.y+y
-    })
-}
-function readPosition(data){
-Position = data.val();
-pointer.x = Position.x;
-pointer.y = Position.y;
-}
-function mouseDragged(){
-    strokeWeight(4);
-    fill(255);
+ 
+ stroke(255);
+ strokeWeight(4);
+ noFill();
+ for(var i = 0; i < drawing.length; i++){
+     var path = drawing[i];
+     beginShape();
+    for(var j = 0; j < path.length; j++){
+    vertex(path[j].x, path[j].y)
+    }
+    endShape();
+ }
+ 
 }
